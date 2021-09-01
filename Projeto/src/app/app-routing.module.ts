ed04f0ad84abc9f70eae444ee9ecb9b260aaa0ea
devnivek <kevin.santos@outlook.com>
2021-09-01 10:10:46 -0300

    tela de sobre

--- Projeto/src/app/app-routing.module.ts
+++ Projeto/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { LogarComponent } from './logar/logar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LogarComponent},
-  {path: 'cadastrar', component: CadastrarComponent}
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'sobre', component: SobreComponent}
 ];
 
 @NgModule({
--- Projeto/src/app/app.module.ts
+++ Projeto/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { LogarComponent } from './logar/logar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
     MenuComponent,
     RodapeComponent,
     LogarComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
